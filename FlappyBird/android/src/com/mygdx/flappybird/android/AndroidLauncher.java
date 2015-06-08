bda23c7fb5a3a472b37154d2945650b12ca71fba
Jun Hyung Park <junhyungandrewpark@gmail.com>
2015-06-07 21:01:15 -0700

    comments

--- FlappyBird/android/src/com/mygdx/flappybird/android/AndroidLauncher.java
+++ FlappyBird/android/src/com/mygdx/flappybird/android/AndroidLauncher.java
@@ -1,15 +1,16 @@
 package com.mygdx.flappybird.android;
 
 import android.os.Bundle;
-
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.mygdx.flappybird.FBGame;
 
-
+/**
+ * This class launches the Android game application
+ */
 public class AndroidLauncher extends AndroidApplication {
 	@Override
-	protected void onCreate (Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		
